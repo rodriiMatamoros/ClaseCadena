380495b59a93db8d54bbe351adb69bac0db9b84c
Rodrigo Matamoros Becerra <rodri@MacBook-Pro-de-Rodrigo.local>
2023-03-23 17:51:53 +0100

    commit Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String iniciales = ClaseCadena.DevuelveIniciales("Andreas Dulac");
+        System.out.println(iniciales); // Imprime "A.D."
+
+        String iniciales2 = ClaseCadena.DevuelveIniciales("Andreas");
+        System.out.println(iniciales2); // Imprime "A."
+
+        String iniciales3 = ClaseCadena.DevuelveIniciales("Andreas Dulac Sánchez");
+        System.out.println(iniciales3); // Imprime "A.D.S."
+
     }
 }
